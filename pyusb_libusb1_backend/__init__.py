d01dd560df7fd648acca4d9fa70eabced090cc28
Benoit Pierre <benoit.pierre@gmail.com>
2021-05-26 00:50:11 +0200

    switch to building libusb as an extension

--- pyusb_libusb1_backend/__init__.py
+++ pyusb_libusb1_backend/__init__.py
@@ -1,6 +1,5 @@
+from importlib.util import find_spec
 
-import sys
-import os
 import usb.backend.libusb1
 
 
@@ -14,14 +13,7 @@ def get_pyusb_backend():
     If there is no custom dylib detected or the platform isn't Mac,
     we return None in order to use pyusb's default behavior.
     """
-
-    if not sys.platform.startswith('darwin'):
+    spec = find_spec('pyusb_libusb1_backend.libusb')
+    if spec is None:
         return None
-
-    dylib_name = 'libusb-1.0.0.dylib'
-    local_libusb1_path = os.path.join(os.path.dirname(__file__), dylib_name)
-
-    if not os.path.exists(local_libusb1_path):
-        return None
-
-    return usb.backend.libusb1.get_backend(find_library=lambda x: local_libusb1_path)
+    return usb.backend.libusb1.get_backend(find_library=lambda x: spec.origin)
--- setup.py
+++ setup.py
@@ -1,26 +1,77 @@
 #!/usr/bin/env python3
 
-__requires__ = '''
-setuptools>=34.4.0
-'''
+import os
+import sys
 
-from setuptools import setup
-import subprocess
-from distutils.cmd import Command
+from setuptools import Extension, setup
 
-class CopyLibusbDylib(Command):
-    description = 'copy the brew-installed libusb1 dylib to the package and make it portable'
-    user_options = []
-    extra_args = []
 
-    def initialize_options(self):
-        pass
+ext_modules = []
 
-    def finalize_options(self):
-        pass
 
-    def run(self):
-        cmd = ['bash', 'copy_libusb.sh']
-        subprocess.check_call(cmd)
+libusb_dir = 'libusb'
+libusb_src_dir = os.path.join(libusb_dir, 'libusb')
 
-setup(cmdclass={'copy_libusb': CopyLibusbDylib})
+libusb_deps = []
+libusb_incs = ['{src}']
+libusb_srcs = ('''
+               {src}/core.c
+               {src}/descriptor.c
+               {src}/hotplug.c
+               {src}/io.c
+               {src}/strerror.c
+               {src}/sync.c
+               '''.split())
+libusb_libs = []
+libusb_ldflags = []
+
+# Only build extension if libusb source tree is present.
+build_libusb_extension = os.path.exists(libusb_src_dir)
+
+# macOS.
+if sys.platform.startswith('darwin'):
+    libusb_deps.extend('''
+                       {src}/os/events_posix.h
+                       {src}/os/threads_posix.h
+                       {src}/os/darwin_usb.h
+                       {top}/Xcode/config.h
+                       '''.split())
+    libusb_incs.insert(0, '{top}/Xcode')
+    libusb_srcs.extend('''
+                       {src}/os/events_posix.c
+                       {src}/os/threads_posix.c
+                       {src}/os/darwin_usb.c
+                       '''.split())
+    libusb_libs.extend('''
+                       objc
+                       '''.split())
+    libusb_ldflags.extend('''
+                          -Wl,-framework,IOKit
+                          -Wl,-framework,CoreFoundation
+                          '''.split())
+else:
+    # Only build extension for supported platforms.
+    build_libusb_extension = False
+
+if build_libusb_extension:
+
+    for var in libusb_deps, libusb_incs, libusb_srcs, libusb_ldflags:
+        for n, v in enumerate(var):
+            var[n] = v.format(
+                top=libusb_dir,
+                src=libusb_src_dir
+            )
+
+    libusb_extension = Extension(
+        'pyusb_libusb1_backend.libusb',
+        depends=libusb_deps,
+        include_dirs=libusb_incs,
+        sources=libusb_srcs,
+        libraries=libusb_libs,
+        extra_link_args=libusb_ldflags,
+    )
+
+    ext_modules.append(libusb_extension)
+
+
+setup(ext_modules=ext_modules)
