46959a060c0ebf715ad4c4d763ab5c3355a4d96e
Benoit Pierre <benoit.pierre@gmail.com>
2021-05-26 01:13:27 +0200

    update documentation

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Pyusb Libusb1 Backend
 
-Currently Mac-only, provides a portable libusb-1.0.0.dylib backend in a wheel for use with [pyusb](https://github.com/pyusb/pyusb).
+Provides a portable libusb backend in a wheel for use with [pyusb](https://github.com/pyusb/pyusb).
--- pyusb_libusb1_backend/__init__.py
+++ pyusb_libusb1_backend/__init__.py
@@ -7,11 +7,11 @@ def get_pyusb_backend():
     """
     pyusb supports a custom backend provided to usb.core.find.
 
-    On macOS, we package libusb and this function will let plugins
-    use the bundled dylib.
+    We package libusb and this function will let plugins use the
+    bundled library.
 
-    If there is no custom dylib detected or the platform isn't Mac,
-    we return None in order to use pyusb's default behavior.
+    If there is no custom library detected we return `None` in
+    order to use pyusb's default behavior.
     """
     spec = find_spec('pyusb_libusb1_backend.libusb')
     if spec is None:
